2c703a73e4ee9296eeca8336936d2343dae64b3f
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-05-23 09:58:58 +0600

    print in c pp

---
+++ 01-Basic-C++/cpp-project/printincpp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hii there\n";
+    int a = 10;
+    std::cout << a;
+    int b = 15;
+    std::cout << std::endl
+              << "My favourite number is" << b;
+    std::cout << std::endl
+              << "My " << b << " favourite number is";
+    return 0;
+}
