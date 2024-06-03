02aaa92095949e400aabed11df3333f3c86062ae
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-06-03 23:38:35 +0600

    take string input with spaces

---
+++ 05-String-Class-In-C++/cpp-project/string_spaces.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n ;
+    cin >> n;
+    cout << n << endl;
+    
+    string s;
+    cin.ignore(); // ignore the newline character
+    getline(cin, s); // read a line of text
+    cout << s;
+    return 0;
+}
