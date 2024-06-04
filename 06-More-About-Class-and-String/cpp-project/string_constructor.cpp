21bb45006f278f238df1227c74d949284ea2739d
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-06-04 14:17:48 +0600

    string constructor

---
+++ 06-More-About-Class-and-String/cpp-project/string_constructor.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s = "hello"; // s = "hello"
+    // cout << s << endl;
+
+    // string s("hello"); // s = "hello"
+    // cout << s << endl;
+
+    // string s("hello", 3); // s = "hel"
+    // cout << s << endl;
+
+    // string a = "hello";
+    // string s(a, 3); // s = "lo"
+    // cout << s << endl;
+
+    // string s("hello", 3, 2); // s = "lo"
+    // cout << s << endl;
+
+    // string s(5, 'a'); // s = "aaaaa"
+    // cout << s << endl;
+
+    return 0;
+}
---
+++ 06-More-About-Class-and-String/cpp-project/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+"; // s = "hello"
+    // cout << s << endl;
