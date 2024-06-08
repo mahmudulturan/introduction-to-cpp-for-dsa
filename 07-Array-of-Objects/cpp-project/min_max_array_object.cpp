2a703870ba4c6f51b282a5311e08dbced01966c7
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-06-08 23:35:28 +0600

    min max array of object.cpp

--- 07-Array-of-Objects/cpp-project/min_max_array_object.cpp
+++ 07-Array-of-Objects/cpp-project/min_max_array_object.cpp
@@ -1,9 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+class Student
 {
-    
-    
+public:
+    string name;
+    int roll;
+    int marks;
+};
+
+main()
+{
+    int n;
+    cin >> n;
+    Student arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i].name >> arr[i].roll >> arr[i].marks;
+    }
+
+    Student min;
+    min.marks = INT_MAX;
+
+    Student max;
+    max.marks = INT_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i].marks < min.marks)
+        {
+            min = arr[i];
+        }
+
+        if (arr[i].marks > max.marks)
+        {
+            max = arr[i];
+        }
+    }
+
+    cout << "Min - " << min.name << " " << min.roll << " " << min.marks << endl;
+    cout << "Max - " << max.name << " " << max.roll << " " << max.marks << endl;
+
     return 0;
 }
